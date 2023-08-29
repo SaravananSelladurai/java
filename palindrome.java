ad697f38587ab4a401c24d0408b66ee7a40bffef
SaravananSelladurai <138362893+SaravananSelladurai@users.noreply.github.com>
2023-08-29 20:02:33 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,1 +1,9 @@
+import java.util.*;
 
+public class palindrome{
+  public static void main(String [] agrs){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number ; ");
+  }
+}
+  
