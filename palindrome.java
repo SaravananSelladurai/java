69efacf213d93510cd6f3cfa881695fbb80efdd3
SaravananSelladurai <saravananak49@gmail.com>
2023-08-30 17:09:09 +0530

    Code is added

--- palindrome.java
+++ palindrome.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class palindrome{
   public static void main(String [] agrs){
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the number ; ");
+    System.out.println("Enter the String  :  ");
+    String str = sc.next();
+    System.out.println("The string is : "+str);
+    sc.close();
+
   }
 }
   
