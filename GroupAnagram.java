3bd2644604faef27fde4734d5da7b2e6422ec5c6
SaravananSelladurai <138362893+SaravananSelladurai@users.noreply.github.com>
2023-11-24 04:25:56 +0000

    Scanner is imported

---
+++ GroupAnagram.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class GroupAnagram{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+        
+    }
+}
